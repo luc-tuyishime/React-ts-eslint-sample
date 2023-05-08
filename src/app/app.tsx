6a0a990353cec4b771f053104d3948db2adec021
luc-tuyishime <luctunechi45@gmail.com>
2023-05-08 17:21:35 +0200

    add new changes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,7 @@
-import styled from 'styled-components';
-
-import NxWelcome from './nx-welcome';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
+import React from 'react';
 
 export function App() {
-  return (
-    <StyledApp>
-      <NxWelcome title="awesomereactapp" />
-    </StyledApp>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
